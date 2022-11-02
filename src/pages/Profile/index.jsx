a86090ecfc39a56a6a8f38305353fc600e88ba97
Wellington José <paivawellington23@gmail.com>
2022-11-02 13:38:41 -0300

    feat: started the profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,11 @@
+import { VStack } from "@chakra-ui/react"
+import { Header } from "../../components/Header"
+
+
+export const ProfilePage = () => {
+    return (
+        <VStack>
+            <Header/>
+        </VStack>
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "../pages/Homepage";
 import { ProductPage } from "../pages/Product";
+import { ProfilePage } from "../pages/Profile";
 
 export const PageRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/product" element={<ProductPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   );
 };
